f367da02e20a6f7db025f42cb02c3d48d0450f07
Дмитрий Калинин <mitya.kalinin1@gmail.com>
2024-02-21 22:47:10 +0700

    meh

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,42 @@ fn main() {
     let file = File::open("input.csv").unwrap();
     let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
     let array_read: Array2<f64> = reader.deserialize_array2_dynamic().unwrap();
+
     println!("{}", array_read);
 
     // получение последнего столбца в отдельный массив
     let last_column_index = array_read.ncols() - 1;
-    let last_column = array_read.column(last_column_index);
-    println!("{}", last_column);
 
-    // получение матрицы без последнего столбца
-    let matrix = array_read.slice(s![.., 0..last_column_index]);
+    let (matrix, last_column) = array_read.view().split_at(Axis(1), last_column_index);
 
-    println!("{}", matrix.column(1));
-    println!("{}", check_for_symmetry(&matrix))
-}
+    let last_column = last_column.to_shape(3).unwrap();
+    println!("last column {}", last_column);
 
-// проверка матрицы на симметричность
-fn check_for_symmetry(matrix: &ArrayView<f64, Dim<[usize; 2]>>) -> bool {
-    matrix == matrix.t()
+    // проверка на квадратность
+    if !matrix.is_square() {
+        println!("Matrix should be squared!");
+        return;
+    }
+
+    // проверка матрицы на симметричность
+    if matrix != matrix.t() {
+        println!("Matrix should be symmetric!");
+        return;
+    }
+
+    let mut g: Vec<f64> = Vec::new();
+
+    for ((i, j), value) in matrix.indexed_iter() {
+        if i == j {
+            println!("{}", value.sqrt());
+            g.push(value.sqrt());
+            continue;
+        }
+        if j > i {
+            continue;
+        }
+        println!("{}", value);
+        g.push((*value - g.iter().map(|el| el.powi(2)).sum::<f64>()));
+    }
+    println!("{:?}", g);
 }
